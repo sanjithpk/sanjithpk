253146170e5c90533ef066d8958e8a425a197ebc
sanjithpk <sanjithpk06@gmail.com>
2021-10-28 14:26:41 +0530

    updating readme

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "Sanjih PK | Developer",
   description: "Exploring efficient ways of developing applications.",
-  subDescription: `I'm currently a student doing a degree in Computer Science and Engineering and love solving real world problems. I have created websites either from the front-end or the back-end mostly using TypeScript, React/Redux, Node.js, Express.js, PostgreSQL, Firebase etc. If you have an exciting project in mind, or would like to simply chat, feel free to do so.`,
+  subDescription: `I'm currently a software engineer and love solving real world problems and passionate about web development and related fields. I have experience in building web applications using React, Gatsby, and NextJS, and mobile applications using React Native.`,
   siteKeywords: "Sanjith PK, Sanjith, sanjithpk, developer, javascript",
   siteUrl: "https://sanjith.me",
   name: "Sanjith PK",
@@ -16,46 +16,46 @@ module.exports = {
     intro: `Hello, I am Sanjith.`,
     para1: `I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide performant and rich experiences to the end users.`,
     para2: `Being a developer is much more than just writing code. Communication skills, a clear mindset, result focused systems, and the ability to set realistic steps to achieve the future result is crucial to a developer's worth.`,
-    techLeft: ["TypeScript", "React", "Node"],
-    techRight: ["Gatsby", "Next", "SQL"]
+    techLeft: ["Akka", "React", "Node"],
+    techRight: ["Material UI", "Next", "TypeScript"],
   },
   socialMedia: [
     {
       name: "Github",
-      url: "https://github.com/sanjithpk"
+      url: "https://github.com/sanjithpk",
     },
     {
       name: "Linkedin",
-      url: "https://www.linkedin.com/in/sanjithpk"
+      url: "https://www.linkedin.com/in/sanjithpk",
     },
     {
       name: "Instagram",
-      url: "https://www.instagram.com/sanjithpk"
+      url: "https://www.instagram.com/sanjithpk",
     },
     {
       name: "Twitter",
-      url: "https://twitter.com/sanjithpk"
-    }
+      url: "https://twitter.com/sanjithpk",
+    },
   ],
   navLinks: [
     {
       name: "Home",
-      url: "/"
+      url: "/",
     },
     {
       name: "About",
-      url: "/about"
+      url: "/about",
     },
     {
       name: "Blog",
-      url: "/blog"
+      url: "/blog",
     },
     {
       name: "Contact",
-      url: "/contact"
-    }
+      url: "/contact",
+    },
   ],
   contact: {
-    api_url: "https://getform.io/f/93b1c53f-0be8-4aa2-ae7c-e16f073c9fc6"
-  }
+    api_url: "https://getform.io/f/93b1c53f-0be8-4aa2-ae7c-e16f073c9fc6",
+  },
 }
